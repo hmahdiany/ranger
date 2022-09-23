b5980254d16884bff5253c49019a4bfbe31a15a8
Hossein Mahdiany <hossein.mahdiany@gmail.com>
2022-09-23 15:07:14 +0330

    add services module to dump all services in cluster

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from v1 import namespaces
 from v1 import configmaps
 from v1 import secrets
+from v1 import services
 from apps_v1 import deployments
 
 def main():
@@ -18,24 +19,29 @@ def main():
 
     # use namespaces package to get all user's namespaces 
     # and create yaml file for them
-    print("exporting all user's namespaces")
+    print("dumping all namespaces in cluster")
     all_ns = namespaces.get_user_namespaces()
     namespaces.create_namespace_yaml_file(all_ns, RESULTS_DIR)
 
     # use configmaps package to dump all configmaps in all namespaces
     # save them in results/<namespace name>/configmaps
-    print("exporting configmaps in all namespaces")
+    print("dumping configmaps in all namespaces")
     configmaps.get_configmaps(all_ns, RESULTS_DIR)
 
     # use secrets package to dump all secrets in all namespaces
     # save them in results/<namespace name>/secrets
-    print("exporting secrets in all namespaces")
+    print("dumping secrets in all namespaces")
     secrets.get_secrets(all_ns, RESULTS_DIR)
 
     # use deployments package to dump all deployments in all namespaces
     # save them in results/<namespace name>/deployments
-    print("exporting deployments in all namespaces")
+    print("dumping deployments in all namespaces")
     deployments.get_deployments(all_ns, RESULTS_DIR)
+
+    # use services package to dump all services in all namespaces
+    # save them in results/<namespace name>/services
+    print("dumping services in all namespaces")
+    services.get_services(all_ns, RESULTS_DIR)
 
 
 if __name__ == "__main__":
---
+++ v1/services.py
@@ -0,0 +1,75 @@
+from . import namespaces as getns
+from pathlib import Path
+import os
+import subprocess
+import json
+import yaml
+from yaml.loader import SafeLoader
+
+def get_services(all_namespaces, result_dir):
+    """
+    this function dumps all services in user's namespaces
+    it uses namespaces module from v1 package
+    """
+
+    # set base directory and create namespaces directory
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    RESULTS_DIR = result_dir
+    SVC_DIR = "services"
+    
+    for ns in all_namespaces:
+
+        # create a directory named services to store all confimaps
+        # in a namespace
+        if os.path.exists(BASE_DIR/RESULTS_DIR/ns/SVC_DIR):
+            print(SVC_DIR, 'directory already exists')
+        else:
+            print('creating', SVC_DIR, 'directory for', ns, 'namespace')
+            os.mkdir(BASE_DIR/RESULTS_DIR/ns/SVC_DIR)
+
+        # create an empty list to list all services in a namespace
+        all_services = []
+
+        # execute command line to append all services in a namespace
+        # to all_services list
+        get_svc = "kubectl get svc -n " + ns + "| grep -v 'NAME' | awk '{print $1}'"
+        svc_cmd = subprocess.Popen(get_svc, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+        output, error = svc_cmd.communicate()
+
+        if error:
+            print("an error occured: ", error.decode('ascii'))
+
+        # append all services in a namespace to all_services list
+        for i in output.decode('ascii').split('\n'):
+            if i != '':
+                all_services.append(i)
+
+        # iterate through all_services list to dump services in a namespace
+        for svc in all_services:
+
+            # execute command line to get user's namespaces in json format
+            # and convert it to yaml format
+            cmd = "kubectl get svc -o json -n " + ns + " " + svc
+            get_svc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+            output, error = get_svc.communicate()
+            try:
+                json_obj = json.loads(output.decode('ascii'))
+                svc_name = json_obj['metadata']['name'] + '.yaml'
+                del json_obj['metadata']['resourceVersion']
+                del json_obj['metadata']['creationTimestamp']
+                del json_obj['metadata']['uid']
+                del json_obj['spec']['clusterIP']
+                del json_obj['spec']['clusterIPs']
+                del json_obj['status']
+                with open(os.path.join(BASE_DIR/RESULTS_DIR/ns/SVC_DIR, svc_name), 'w') as f:
+                    yaml.dump(json_obj, f)
+            except:
+                print(error)
+
+if __name__ == "__main__":
+
+    # define result directory
+    RESULTS_DIR = "results"
+
+    all_ns = getns.get_user_namespaces()
+    get_services(all_ns, RESULTS_DIR)
